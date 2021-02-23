6831c5f518ac19e2f711d73d893acc44fab3ca86
Antonygil99 <68798029+Antonygil99@users.noreply.github.com>
2021-02-23 12:31:53 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
-
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
 #include "project.h"
 #include "stdlib.h"
 #include "math.h"
@@ -10,9 +20,11 @@
 //.......global variables.....
 //............................
 uint8 i = 0;        //interruption flag
-uint16 output = 0; //ADC gotten value
+uint16 output[2]; //ADC gotten value
 uint16 freq = 0;  //Varable for the frequency mapping
 uint16 mapfreq = 0;
+
+long delaytime = 0;//measures the trigger time
 
 
 //..............................
@@ -20,48 +32,75 @@ uint16 mapfreq = 0;
 //..............................
 long map(long x, long in_min, long in_max, long out_min, long out_max);
 
-//PROTO FUNCTION FOR SWITCH INTERRUPT
-CY_ISR_PROTO(duty);// Duty cycles from PWM1 and PWM2 are modified
+//..............................
+//.....interruotion dimmer......
+//.............................
+
+CY_ISR_PROTO(angle_set);
+
+//..............................
+//.....ADC FREQUENCY LECT......
+//.............................
+
+void adc_frequency();
+void adc_triggerLect();
+
 
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
-    isr_duty_StartEx(duty);
-    isr_duty_ClearPending();
-    
+    isr_triggerAngle_StartEx(angle_set);
+    isr_triggerAngle_ClearPending();    
     PWM_1_Start();
-    PWM_2_Start();
+    
+    
     OPAMP_Start();
     ADC_Start();
-    ADC_StartConvert();
-    
+    AMux_Start();
     I2COLED_Start();
     display_init(DISPLAY_ADDRESS);
    
     for(;;)
     {
+       adc_frequency();
+       adc_triggerLect();
         
-        if(ADC_IsEndConversion(ADC_RETURN_STATUS))
-        {
-            CyDelay(20);
-            output = ADC_GetResult8();
-            
-            freq = map(output,0,127,132,1332);
-            mapfreq = map(output,0,127,700,70);
-             /*Saturate ADC result to positive numbers. */
-            if(output <= 0)
-            {
-                output = 0;
-            }
-
-            Clock_SetDividerRegister(freq,1);//sets the devider register from 1132(70Hz) to 132(700Hz) 
-       //modifies the period of both PWM1 and PWM2
-     
-       }
        
-        
-       uint16 devregclock = Clock_GetDividerRegister();//gets the divider register value
+    }
+}
+
+
+
+CY_ISR(angle_set)
+{
+    CyDelay(delaytime);
+    dimmerout_Write(1);
+    CyDelayUs(10);
+    dimmerout_Write(0);
+    
+    triggerAngle_ClearInterrupt();
+    
+}
+
+void adc_frequency()
+{
+    AMux_FastSelect(0);
+        ADC_StartConvert();
+        ADC_IsEndConversion(ADC_WAIT_FOR_RESULT);
+        output[0] = ADC_GetResult8();
+        ADC_StopConvert();
+            
+            freq = map(output[0],0,127,132,1332);
+            mapfreq = map(output[0],0,127,700,70);
+             /*Saturate ADC result to positive numbers. */
+            if(output[0] <= 0)
+            {
+                output[0] = 0;
+            }
+       Clock_SetDividerRegister(freq,1);//sets the devider register from 1132(70Hz) to 132(700Hz) 
+       //modifies the period of both PWM1 and PWM2
+      // uint16 devregclock = Clock_GetDividerRegister();//gets the divider register value
         
        //this part of the code corresponds to the OLED display I2C module 
        //which has some bugs those which I am Working yet.
@@ -78,116 +117,20 @@ int main(void)
        gfx_setCursor(65,20);
        gfx_println("Hz");
        display_update();
-      // CyDelay(200);
-        
-       
-    }
 }
 
-
-CY_ISR(duty)
+void adc_triggerLect()
 {
-
-    CyDelay(300);
-    i++;
-    
-    if(i==1)        // DUTY 10%
-    {
-      PWM_1_WritePeriod(25); 
-      PWM_2_WritePeriod(25); 
-      
-        
-      
-       display_clear();    
-       display_update();    
-       gfx_setTextSize(3);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(2,5);
-       gfx_println("10%");
-       gfx_setTextSize(1);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(60,20);
-       gfx_println("duty");
-       display_update();
-    }
-    else if(i == 2) // DUTY 20%
-    {
-      PWM_1_WritePeriod(51); 
-      PWM_2_WritePeriod(51); 
-    
-       display_clear();    
-       display_update();    
-       gfx_setTextSize(3);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(2,5);
-       gfx_println("20%");
-       gfx_setTextSize(1);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(60,20);
-       gfx_println("duty");
-       display_update();
-    }
-    else if(i == 3) // DUTY 30%
-    {
-      PWM_1_WritePeriod(77); 
-      PWM_2_WritePeriod(77);
-     
-       display_clear();    
-       display_update();    
-       gfx_setTextSize(3);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(2,5);
-       gfx_println("30%");
-       gfx_setTextSize(1);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(60,20);
-       gfx_println("duty");
-       display_update();
-    }
-    else if(i == 4) // DUTY 40%
-    {
-      PWM_1_WritePeriod(102); 
-      PWM_2_WritePeriod(102); 
-      
-       display_clear();    
-       display_update();    
-       gfx_setTextSize(3);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(2,5);
-       gfx_println("40%");
-       gfx_setTextSize(1);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(60,20);
-       gfx_println("duty");
-       display_update();
-    }
-    else if(i == 5) // DUTY 50%
-    {
-      PWM_1_WritePeriod(127); 
-      PWM_2_WritePeriod(127); 
-      
-      display_clear();    
-       display_update();    
-       gfx_setTextSize(3);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(2,5);
-       gfx_println("50%");
-       gfx_setTextSize(1);
-       gfx_setTextColor(WHITE);
-       gfx_setCursor(60,20);
-       gfx_println("duty");
-       display_update();
-    }
-    
-    
-    if(i>5)
-    {
-        i = 0;
-    }
-    duty_ClearInterrupt();
-    
-    
+    AMux_FastSelect(1);
+    ADC_StartConvert();
+    ADC_IsEndConversion(ADC_WAIT_FOR_RESULT);
+    output[1] = ADC_GetResult8();
+    ADC_StopConvert();
+            
+    delaytime = map(output[1],0,255,0,8);   
 }
+
+
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
     long divisor = (in_max - in_min);
     if(divisor == 0){
@@ -197,3 +140,4 @@ long map(long x, long in_min, long in_max, long out_min, long out_max) {
 }
 
 /* [] END OF FILE */
+
